b421670f3f67184a6d75099473ccfb6c70707f9d
ai73aaa <1qazxcvBG>
2020-05-29 17:26:31 +0300

    add getGeneralChatMessages

--- src/cnb-2-features/f-3-social/index.ts
+++ src/cnb-2-features/f-3-social/index.ts
@@ -2,10 +2,12 @@ import express from "express";
 import {findUserByToken} from "../f-1-auth/a-3-helpers/h-2-users/findUserByToken";
 import {getUsers} from "./s-1-controllers/getUsers";
 import {addGeneralMessage} from "./s-1-controllers/addGeneralMessage";
+import {getGeneralMessages} from "./s-1-controllers/getGeneralMessages";
 
 const social = express.Router();
 
 social.get('/users', findUserByToken(getUsers, 'getUsers', true));
 social.post('/general/message', findUserByToken(addGeneralMessage, 'addGeneralMessage'));
+social.get('/general/message', findUserByToken(getGeneralMessages, 'getGeneralMessages', true));
 
 export default social;
