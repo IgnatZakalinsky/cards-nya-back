73c21bc6d8c780f55ee6e070eece96cdef59f601
ai73aaa <1qazxcvBG>
2020-06-12 17:17:40 +0300

    add get user by id

--- dist/cnb-2-features/f-3-social/index.js
+++ dist/cnb-2-features/f-3-social/index.js
@@ -8,8 +8,10 @@ const findUserByToken_1 = require("../f-1-auth/a-3-helpers/h-2-users/findUserByT
 const getUsers_1 = require("./s-1-controllers/getUsers");
 const addGeneralMessage_1 = require("./s-1-controllers/addGeneralMessage");
 const getGeneralMessages_1 = require("./s-1-controllers/getGeneralMessages");
+const getUser_1 = require("./s-1-controllers/getUser");
 const social = express_1.default.Router();
 social.get('/users', findUserByToken_1.findUserByToken(getUsers_1.getUsers, 'getUsers', true));
+social.get('/user', findUserByToken_1.findUserByToken(getUser_1.getUser, 'getUser', true));
 social.post('/general/message', findUserByToken_1.findUserByToken(addGeneralMessage_1.addGeneralMessage, 'addGeneralMessage'));
 social.get('/general/message', findUserByToken_1.findUserByToken(getGeneralMessages_1.getGeneralMessages, 'getGeneralMessages', true));
 exports.default = social;
